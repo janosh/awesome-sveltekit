6dfce18cdd4f50e380c686bf2b00f9e3dc0f0a17
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-09-25 19:31:50 +0200

    finish webp transition, add repo links

--- site/src/components/SiteList.svelte
+++ site/src/components/SiteList.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
   import type { Site } from '../global'
   import Modal from '../components/Modal.svelte'
+  import MarkGithub from '@svicons/octicons/mark-github.svelte'
 
   export let sites: Site[]
 
   let activeSite: string | null = null
 
-  const prettyDate = (date = ``): string => new Date(date).toISOString().split(`T`)[0]
+  const prettyDate = (date = ``): string =>
+    new Date(date).toLocaleString(`en`, {
+      month: `short`,
+      year: `numeric`,
+      day: `numeric`,
+    })
 </script>
 
 <ol>
-  {#each sites as { title, url, tags, creator, creatorTwitter, creatorUrl, dateCreated, description } (url)}
+  {#each sites as { title, url, tags, creator, creatorTwitter, creatorUrl, dateCreated, description, repo } (url)}
     <li>
       <img
         on:click={() => (activeSite = url)}
-        src="/screenshots/{title.toLowerCase().replaceAll(` `, `-`)}.png"
+        src="/screenshots/{title.toLowerCase().replaceAll(` `, `-`)}.webp"
         alt="Screenshot of {title}" />
       <a href={url} on:click|stopPropagation>{title}</a><br />
       <small>{tags.join(`, `)}</small>
@@ -22,21 +28,29 @@
     {#if activeSite === url}
       <Modal on:close={() => (activeSite = null)} style="background: #2c2b35;">
         <div>
-          <h1>
+          <h1
+            style="display: flex; align-items: center; gap: 1em; justify-content: space-between;">
             <a href={url}>{title}</a>
+            {#if repo}
+              <small style="display: flex; align-items: center; gap: 6pt;">
+                <MarkGithub width="1.2em" /><a href={repo}>Code</a>
+              </small>
+            {/if}
           </h1>
           <img
             src="/screenshots/{title.toLowerCase().replaceAll(` `, `-`)}.webp"
             alt={title} />
-          <p>{description}</p>
+          {#if description}
+            <p>{description}</p>
+          {/if}
           <p>
             Creator: {creator}
             {#if creatorUrl}
-              &nbsp;&mdash;&nbsp; <a href={creatorUrl}>Web</a>
+              &nbsp;&mdash;&nbsp;<a href={creatorUrl}>Web</a>
             {/if}
             {#if creatorTwitter}
-              &nbsp;&mdash;&nbsp; <a href="https://twitter.com/@{creatorTwitter}"
-                >Twitter</a>
+              &nbsp;&mdash;&nbsp;<a href="https://twitter.com/@{creatorTwitter}">
+                Twitter</a>
             {/if}
           </p>
           {#if dateCreated}
@@ -61,6 +75,8 @@
   }
   ol li img {
     cursor: pointer;
+  }
+  img {
     width: 100%;
   }
 </style>
