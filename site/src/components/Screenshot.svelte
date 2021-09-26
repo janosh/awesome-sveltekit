2e2381230186c77956aed10edd26f34ee671fda0
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-09-26 12:27:14 +0100

    stop layout shift with explicit width+height img attrs, add GH repo stars

---
+++ site/src/components/Screenshot.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let title: string
+  export let width = 800
+  export let height = 600
+
+  const src = `/screenshots/${title.toLowerCase().replaceAll(` `, `-`)}.webp`
+</script>
+
+<img {src} alt="Screenshot of {title}" {width} {height} on:click />
+
+<style>
+  img {
+    width: 100%;
+    height: auto;
+  }
+</style>
