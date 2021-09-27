6e0fa4f2416e7deb3c325d26810deeb290164020
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-09-27 20:50:43 +0100

    fetch repo stars in parseSitesYaml.js

--- site/src/scripts/parseSitesYaml.js
+++ site/src/scripts/parseSitesYaml.js
@@ -1,9 +1,19 @@
 /* eslint-disable no-console */
 import yaml from 'js-yaml'
+import fetch from 'node-fetch'
 
 import fs from 'fs'
 
 const sites = yaml.load(fs.readFileSync(`../sites.yml`))
+
+for (const site of sites) {
+  if (!site.repo) continue
+
+  const repoHandle = site.repo.split(`github.com/`)[1]
+  const response = await fetch(`https://api.github.com/repos/${repoHandle}`)
+  const json = await response.json()
+  site.repoStars = json.stargazers_count
+}
 
 fs.writeFileSync(
   `src/sites.ts`,
