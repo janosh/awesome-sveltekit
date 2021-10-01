84556d2a2393c473c7367b5b7ca0318875bab044
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-10-01 19:38:54 +0100

    mv site/src/scripts -> site/scripts

--- site/scripts/index.js
+++ site/scripts/index.js
@@ -2,10 +2,10 @@ import { fork } from 'child_process'
 import { URL } from 'url'
 import { dirname } from 'path'
 
-export const rootDir = dirname(new URL(`../..`, import.meta.url).pathname)
+export const rootDir = dirname(new URL(`..`, import.meta.url).pathname)
 
 if (import.meta.url === `file://${process.argv[1]}`) {
   // module was not imported but called directly
-  fork(`src/scripts/parseSitesYaml.js`)
-  fork(`src/scripts/takeScreenshots.js`)
+  fork(`${rootDir}/site/scripts/parseSitesYaml.js`)
+  fork(`${rootDir}/site/scripts/takeScreenshots.js`)
 }
