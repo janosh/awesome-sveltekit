b4812ec5b47b18533e7a0757a54b4a8f0408df20
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-03-22 09:02:48 +0000

    Add site 45 https://houdinigraphql.com (#67)
    * update deps
    
    * address sveltekit breaking changes
    
    * add site 45 https://houdinigraphql.com
    
    * clean up input[type=search] and multiselect CSS

--- site/src/routes/[slug].ts
+++ site/src/routes/[slug].ts
@@ -5,5 +5,5 @@ export const get: RequestHandler = ({ params }) => {
   const site = sites.find((itm) => itm.slug === params.slug)
 
   if (site) return { body: { site } }
-  else return { fallthrough: true }
+  else return { status: 404 }
 }
