bca2443fede03e9a376208a974f20fe6b5246707
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-06-02 19:38:36 +0100

    add https://github.com/oslabs-beta/Svelvet

--- site/src/app.d.ts
+++ site/src/app.d.ts
@@ -4,3 +4,8 @@ declare module '*sites.yml' {
   const sites: import('./types').Site[]
   export default sites
 }
+
+declare module '*uses-links.yml' {
+  const links: Record<string, string>
+  export default links
+}
