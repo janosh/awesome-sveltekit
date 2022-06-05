a8dba2a017e96186e67ba00b694bf7286c209a24
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-06-05 10:45:12 +0100

    SiteList + SiteDetails CSS tweaks

--- site/src/components/SiteList.svelte
+++ site/src/components/SiteList.svelte
@@ -45,8 +45,16 @@
     list-style: none;
     padding: 0;
     display: grid;
-    grid-gap: 2em;
     grid-template-columns: repeat(auto-fill, minmax(15em, 1fr));
+  }
+  ol li {
+    transition: 0.3s;
+    border-radius: 1ex;
+    padding: 10pt;
+  }
+  ol li:hover {
+    transform: scale(1.01);
+    background-color: rgba(255, 255, 255, 0.05);
   }
   div.flex {
     margin-top: 6pt;
