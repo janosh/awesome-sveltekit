9fd45f6c2e1765bf3b861e64d569b5cadaf8c8b1
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-07-13 12:10:29 +0200

    add svelte:head title, desc and og/twitter SEO meta tags

--- site/src/routes/[slug].svelte
+++ site/src/routes/[slug].svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
   import SiteDetails from '../components/SiteDetails.svelte'
   import { Site } from '../types'
+
   export let site: Site
+
+  $: head_title = `${site.title} | Awesome SvelteKit`
+  $: plain_description = site?.description?.replace(/<[^>]*>/g, ``)
 </script>
+
+<svelte:head>
+  <title>{head_title}</title>
+  <meta property="og:title" content={head_title} />
+  {#if plain_description}
+    <meta name="description" content={plain_description} />
+    <meta property="og:description" content={plain_description} />
+  {/if}
+  {#if site.contributors[0]?.twitter}
+    <meta name="twitter:creator" content={site.contributors[0].twitter} />
+  {/if}
+</svelte:head>
 
 <a href="/" class="back" sveltekit:prefetch>&laquo; back</a>
 <SiteDetails {site} />
