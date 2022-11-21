a28cc39bf46b72fbbfcca7d45c3fde8462f4553c
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-11-20 18:09:01 -0800

    fix breaking changes to +error.svelte API

--- site/src/routes/+error.svelte
+++ site/src/routes/+error.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { dev } from '$app/environment'
   import { page } from '$app/stores'
 </script>
 
@@ -8,20 +7,9 @@
 </svelte:head>
 
 <div>
-  {#if $page.status === 404}
-    <h1>⛔ {$page.error?.name} {$page.status}: Page not found</h1>
-    <p>
-      Return to the
-      <a href="/">landing page</a>.
-    </p>
-  {:else}
-    <h1>⛔ {$page.error?.name} {$page.status}</h1>
-  {/if}
+  <h1>Error {$page.status}: {$page.error?.message}</h1>
 
-  {#if dev && $page.error?.stack}
-    <h2>Stack Trace</h2>
-    <pre>{$page.error.stack}</pre>
-  {/if}
+  <p>Back to <a href=".">landing page</a>.</p>
 </div>
 
 <style>
@@ -36,13 +24,5 @@
     text-align: center;
     max-width: 35em;
     margin: auto;
-  }
-  pre {
-    overflow: scroll;
-    font-size: 0.9em;
-    white-space: pre-wrap;
-    background: var(--accentBg);
-    padding: 5pt 1em;
-    border-radius: 3pt;
   }
 </style>
