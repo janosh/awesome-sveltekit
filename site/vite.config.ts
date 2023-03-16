d42efaa30e77364b728fa80c6355c95b6e1e0dd3
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-03-16 09:54:17 -0700

    pnpm add -D sharp to convert all screenshots from WebP to AVIF

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -2,6 +2,11 @@ import yaml from '@rollup/plugin-yaml'
 import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
 
+const run_script = process.argv.some((arg) => arg.startsWith(`screenshots:`))
+if (run_script) {
+  await import(`./scripts/screenshots.ts`)
+}
+
 export default {
   plugins: [sveltekit(), yaml()],
 
